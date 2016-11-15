f8692278ecdb0ae76b545b5965d548e6b4ed8288
andy jenkins <andygjenkins@gmail.com>
2016-11-15 17:41:45 +0000

    Fix cheeky devise issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  root "restaurants#index"
+
   devise_for :users
   resources :restaurants
   get 'restaurants' => 'restaurants#index'
 
   resources :restaurants do
-    resources :reviews    
+    resources :reviews
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
