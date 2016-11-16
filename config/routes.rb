70118c8d3421b8855e911061bb085abacad98dc3
antonyCastineiras <antonycastineiras80@gmail.com>
2016-11-16 12:18:04 +0000

    facebook login not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  get 'users/facebook'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- resources :restaurants do
- 	resources :reviews
- end
+  root 'restaurants#index'
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_scope :user do
+ 	  #delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
  
- get 'restaurants' => 'restaurants#index'
+  resources :restaurants do
+ 	 resources :reviews
+  end
+ 
+  get 'restaurants' => 'restaurants#index'
 end
